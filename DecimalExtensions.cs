280282263d13f283fd4e652086ff3d1ff0252a13
Daniel B <beckdp01@gmail.com>
2024-08-24 08:38:22 -0700

    added extension method for decimal to show reduced price

---
+++ DecimalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public static class DecimalExtensions
+    {
+
+        public static decimal DiscountThisPrice(this decimal value)
+        {
+            return Math.Round(value * 0.9m, 2);
+        }
+    }
+}
